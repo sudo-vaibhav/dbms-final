0564f7e05eda35d5ead22fc4f078bd15ed0ac7f8
NeelChoksi <neelchoksi1611@gmail.com>
2020-10-29 17:13:47 +0530

    jailor can change section of prisoner

--- criminal_system/jailor-dashboard.php
+++ criminal_system/jailor-dashboard.php
@@ -78,7 +78,7 @@
               <div class="flex-grow pl-6">
                 <h2 class="text-gray-900 text-lg title-font font-medium mb-2">Update Prisoners Section ID</h2>
                 <p class="leading-relaxed text-base">Update section of prisoners stored in the database.</p>
-                <a class="mt-3 text-indigo-500 inline-flex items-center">Update Prisoners Section ID
+                <a href="./prisoner_newsection.php" class="mt-3 text-indigo-500 inline-flex items-center">Update Prisoners Section ID
                   <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                   </svg>
