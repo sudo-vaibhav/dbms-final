c086e4b95b5568d132126496dc32a3f083ac56e5
deepcr7 <deepeshsuranjan@gmail.com>
2020-10-29 15:00:53 +0530

    made minor change in aboutus page

--- criminal_system/aboutus.php
+++ criminal_system/aboutus.php
@@ -2,7 +2,7 @@
 require "./header.php";
 ?>
 <div class="flex flex-col h-screen">
-  <section class="text-gray-700 body-font">
+<section class="text-gray-700 body-font">
     <div class="container px-5 py-24 mx-auto">
       <div class="flex flex-col text-center w-full mb-20">
         <h1 class="text-2xl font-medium title-font mb-4 text-gray-900 tracking-widest">OUR TEAM</h1>
@@ -11,7 +11,7 @@ require "./header.php";
       <div class="flex flex-wrap -m-4">
         <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
           <div class="h-full text-center">
-            <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="./assets/vaibhav.jpg">
+            <img alt="testimonial" class="w-60 h-40 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="./assets/vaibhav.jpg">
             <p class="leading-relaxed">Hi! I'm Vaibhav Chopra, a student of Vellore Institute Of Technology pursuing my Undergraduate Degree in Computer Science and Engineering.</p>
             <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
             <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">VAIBHAV CHOPRA</h2>
@@ -20,7 +20,7 @@ require "./header.php";
         </div>
         <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
           <div class="h-full text-center">
-            <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/300x300">
+            <img alt="testimonial" class="w-60 h-40 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="./assets/neel.jpeg">
             <p class="leading-relaxed">Hi! I'm Neel Rakesh Choksi, a student of Vellore Institute Of Technology pursuing my Undergraduate Degree in Computer Science and Engineering.</p>
             <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
             <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">NEEL RAKESH CHOKSI</h2>
@@ -29,7 +29,7 @@ require "./header.php";
         </div>
         <div class="lg:w-1/3 lg:mb-0 p-4">
           <div class="h-full text-center">
-            <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/305x305">
+            <img alt="testimonial" class="w-60 h-40 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="./assets/deepesh.jpeg">
             <p class="leading-relaxed">Hi! I'm Deepesh Suranjandass, a student of Vellore Institute Of Technology pursuing my Undergraduate Degree in Computer Science and Engineering.</p>
             <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
             <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">Deepesh Suranjandass</h2>
